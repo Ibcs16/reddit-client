9858dd05302b49e63d3336d62886f88e1944f05c
Iago Brayham <iagobrayhamcunha@hotmail.com>
2021-06-14 02:09:30 -0300

    fix app test name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("loads react app", () => {
   render(<App />);
 
   expect(screen.getByRole("heading")).toBeInTheDocument();
