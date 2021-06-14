af8c15e89e24c0b40eb04432d7f9b66c65f9ee63
Iago Brayham <iagobrayhamcunha@hotmail.com>
2021-06-14 02:16:36 -0300

    clear app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,6 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
-test("loads react app", () => {
+test("should render app", () => {
   render(<App />);
-
-  expect(screen.getByRole("heading")).toBeInTheDocument();
 });
